a9ee8afacad90e742ee18ab1618c90ac27edb8c4
shuan24 <31192268+shuan24@users.noreply.github.com>
2026-08-25 15:29:26 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-# Replace with your actual PythonAnywhere path.
-project_home = "/home/YOUR_USERNAME/indian_salary_calculator/backend"
-
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-from app import app as application
